11aeb0473a163d246e83bd6c9be9a5855623c400
Andreas Trepczik <unmacaque@gmail.com>
2014-12-18 20:21:41 +0100

    add junit test

---
+++ src/test/java/com/gmail/unmacaque/springsecurity/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.gmail.unmacaque.springsecurity;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:servlet-context.xml")
+public class HelloControllerTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void testRoot() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(status().isOk());
+	}
+}
