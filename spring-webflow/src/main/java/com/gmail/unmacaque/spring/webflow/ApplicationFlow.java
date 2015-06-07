c099f4bd6dfab022efa18eeeae83e8b5d18bcbae
Andreas Trepczik <andy@asustek.fritz.box>
2015-06-07 15:47:28 +0200

    correctly configured spring-webflow

--- spring-webflow/src/main/java/com/gmail/unmacaque/spring/webflow/ApplicationFlow.java
+++ spring-webflow/src/main/java/com/gmail/unmacaque/spring/webflow/ApplicationFlow.java
@@ -1,6 +1,6 @@
 package com.gmail.unmacaque.spring.webflow;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +12,16 @@ import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.executor.FlowExecutor;
 import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 import org.springframework.webflow.security.SecurityFlowExecutionListener;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 @Configuration
 public class ApplicationFlow extends AbstractFlowConfiguration {
 
 	@Autowired
-	private ViewResolver viewResolver;
+	private ThymeleafViewResolver viewResolver;
 
 	@Bean
 	public FlowExecutor flowExecutor() {
@@ -27,10 +30,26 @@ public class ApplicationFlow extends AbstractFlowConfiguration {
 	}
 
 	@Bean
+	public FlowHandlerMapping flowHandlerMapping() {
+		FlowHandlerMapping flowHandlerMapping = new FlowHandlerMapping();
+		flowHandlerMapping.setOrder(0);
+		flowHandlerMapping.setFlowRegistry(flowRegistry());
+		return flowHandlerMapping;
+	}
+
+	@Bean
+	public FlowHandlerAdapter flowHandlerAdapter() {
+		FlowHandlerAdapter handlerAdapter = new FlowHandlerAdapter();
+		handlerAdapter.setFlowExecutor(flowExecutor());
+		handlerAdapter.setSaveOutputToFlashScopeOnRedirect(true);
+		return handlerAdapter;
+	}
+
+	@Bean
 	public FlowDefinitionRegistry flowRegistry() {
 		return getFlowDefinitionRegistryBuilder(flowBuilderServices())
 				.setBasePath("classpath:flows")
-				.addFlowLocationPattern("/**/*-flow.xml").build();
+				.addFlowLocationPattern("/*.xml").build();
 	}
 
 	@Bean
@@ -43,7 +62,7 @@ public class ApplicationFlow extends AbstractFlowConfiguration {
 	@Bean
 	public MvcViewFactoryCreator mvcViewFactoryCreator() {
 		MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
-		factoryCreator.setViewResolvers(Collections.singletonList(viewResolver));
+		factoryCreator.setViewResolvers(Arrays.<ViewResolver>asList(viewResolver));
 		factoryCreator.setUseSpringBeanBinding(true);
 		return factoryCreator;
 	}
