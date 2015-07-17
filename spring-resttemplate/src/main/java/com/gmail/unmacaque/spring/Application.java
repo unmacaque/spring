d1bf78831c1b84a3abec873a8a1a90f75dce1d7e
Andreas Trepczik <andy@asustek.fritz.box>
2015-07-17 20:44:15 +0200

    add spring-resttemplate project

---
+++ spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -0,0 +1,52 @@
+package com.gmail.unmacaque.spring;
+
+import java.util.EventListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.github.tomakehurst.wiremock.jetty6.Jetty6HandlerDispatchingServlet;
+import com.github.tomakehurst.wiremock.servlet.WireMockWebContextListener;
+import com.gmail.unmacaque.spring.resttemplate.Bundle;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private static final Logger logger = LogManager.getRootLogger();
+
+	public Jetty6HandlerDispatchingServlet wireMockServlet() {
+		return new Jetty6HandlerDispatchingServlet();
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<EventListener> servletListenerRegistrationBean() {
+		return new ServletListenerRegistrationBean<EventListener>(new WireMockWebContextListener());
+	}
+
+	@Bean
+	public ServletRegistrationBean wireMockRegistrationBean() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(wireMockServlet(), "/*");
+		registration.addInitParameter("RequestHandlerClass", "com.github.tomakehurst.wiremock.http.StubRequestHandler");
+		return registration;
+	}
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		context.close();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		Bundle bundle = restTemplate.getForObject("http://localhost:8080/", Bundle.class);
+		logger.info(bundle);
+	}
+}
---
+++ spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/resttemplate/Bundle.java
@@ -0,0 +1,22 @@
+package com.gmail.unmacaque.spring.resttemplate;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Bundle {
+	private final String content;
+
+	@JsonCreator
+	public Bundle(@JsonProperty("content") String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	@Override
+	public String toString() {
+		return content;
+	}
+}
