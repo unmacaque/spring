615d7b9e7e96f462fc254f30a2115ed38fb3678b
Andreas Trepczik <andy@asustek.fritz.box>
2015-08-22 09:58:20 +0200

    add spring-upload project

---
+++ spring-upload/src/main/java/com/gmail/unmacaque/spring/upload/UploadController.java
@@ -0,0 +1,70 @@
+package com.gmail.unmacaque.spring.upload;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping(value = "/")
+public class UploadController {
+
+	private final Path tempDirectory;
+
+	public UploadController() throws IOException {
+		Set<PosixFilePermission> permissionSet = PosixFilePermissions.fromString("rwxr-xr-x");
+		FileAttribute<?> fileAttribute = PosixFilePermissions.asFileAttribute(permissionSet);
+		tempDirectory = Files.createTempDirectory("upload", fileAttribute);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getUpload() {
+		return "index";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String postUpload(@RequestParam("aFile") MultipartFile file, ModelMap modelMap) {
+		if (file.isEmpty()) {
+			modelMap.addAttribute("error", "File is empty");
+			return "index";
+		}
+		try {
+			byte[] bytes = file.getBytes();
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(tempDirectory.toFile(), file.getOriginalFilename()));
+			BufferedOutputStream stream = new BufferedOutputStream(fileOutputStream);
+			stream.write(bytes);
+			stream.close();
+			modelMap.addAttribute("filename", file.getOriginalFilename());
+			modelMap.addAttribute("filesize", file.getSize());
+			modelMap.addAttribute("filetype", file.getContentType());
+		} catch (Exception e) {
+			modelMap.addAttribute("error", e.getMessage());
+		}
+		return "index";
+	}
+
+	@RequestMapping(value = "/download", method = RequestMethod.GET, produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
+	@ResponseBody
+	public FileSystemResource download(@RequestParam("filename") String filename, HttpServletResponse response) {
+		response.setHeader("Content-Disposition", "attachment;filename=" + filename);
+		return new FileSystemResource(tempDirectory + "/" + filename);
+	}
+}
