1987d1a9639d2707673e3448f2d9fbbfb2bf0120
Andreas Trepczik <andy@asustek.fritz.box>
2016-01-07 16:40:02 +0100

    fix parent pom.xml, add spring-boot-starter-test
    spring-rest: improve integration test

--- spring-rest/src/test/java/com/gmail/unmacaque/spring/rest/GreetControllerTest.java
+++ spring-rest/src/test/java/com/gmail/unmacaque/spring/rest/GreetControllerTest.java
@@ -10,16 +10,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.gmail.unmacaque.spring.Application;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration("classpath:servlet-context.xml")
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest
 public class GreetControllerTest {
 
 	@Autowired
@@ -29,7 +31,7 @@ public class GreetControllerTest {
 
 	@Before
 	public void setup() {
-		this.mockMvc = webAppContextSetup(this.webApplicationContext)
+		this.mockMvc = webAppContextSetup(webApplicationContext)
 				.addFilters(new CorsFilterBean())
 				.build();
 	}
