233751402ea829b7a632ca72698c2543699df926
Andreas Trepczik <andy@asustek.fritz.box>
2016-01-24 12:37:43 +0100

    update hibernate to 5.0.7.Final

--- spring-webflow/src/main/java/com/gmail/unmacaque/spring/webflow/ApplicationFlow.java
+++ spring-webflow/src/main/java/com/gmail/unmacaque/spring/webflow/ApplicationFlow.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.webflow.config.AbstractFlowConfiguration;
--- spring-webflow/src/main/java/com/gmail/unmacaque/spring/webflow/order/HsqlOrderRepository.java
+++ spring-webflow/src/main/java/com/gmail/unmacaque/spring/webflow/order/HsqlOrderRepository.java
@@ -2,11 +2,10 @@ package com.gmail.unmacaque.spring.webflow.order;
 
 import java.util.Collection;
 
-import javax.transaction.Transactional;
-
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
