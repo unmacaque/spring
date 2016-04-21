584228489d2107af7d415df98c249a47f3a8a108
Andreas Trepczik <andy@asustek.fritz.box>
2016-04-21 22:34:46 +0200

    greatly simplify pom.xml
    achieved by removing dependencies which spring-boot-starter-data-jpa
    provides

--- spring-angularjs/src/test/java/com/gmail/unmacaque/spring/angularjs/TaskRestControllerTest.java
+++ spring-angularjs/src/test/java/com/gmail/unmacaque/spring/angularjs/TaskRestControllerTest.java
@@ -6,30 +6,25 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(TaskRestControllerTest.Config.class)
-@WebIntegrationTest
+@WebMvcTest
 public class TaskRestControllerTest {
 
 	@Configuration
@@ -43,18 +38,11 @@ public class TaskRestControllerTest {
 	}
 
 	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	@Autowired
 	@Qualifier("taskRepository")
 	private TaskRepository taskRepositoryMock;
 
+	@Autowired
 	private MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		this.mockMvc = webAppContextSetup(webApplicationContext).build();
-	}
 
 	@After
 	public void cleanup() {
