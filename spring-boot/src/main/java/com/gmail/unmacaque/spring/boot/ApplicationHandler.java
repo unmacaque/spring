61b10f8ccc766554fc72187449d73a782958b3bc
Andreas Trepczik <andy@asustek.fritz.box>
2016-05-02 23:29:36 +0200

    spring-boot: simplify project, make use of ApplicationRunner

---
+++ spring-boot/src/main/java/com/gmail/unmacaque/spring/boot/ApplicationHandler.java
@@ -0,0 +1,24 @@
+package com.gmail.unmacaque.spring.boot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationHandler implements ApplicationRunner {
+
+	@Value("${application.my-property}")
+	private String myProperty;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(myProperty);
+		System.out.println(args.getNonOptionArgs());
+
+		for (String optionName : args.getOptionNames()) {
+			System.out.println(optionName + "=" + args.getOptionValues(optionName));
+		}
+	}
+
+}
