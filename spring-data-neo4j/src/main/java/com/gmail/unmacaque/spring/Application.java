221d5e611af45eca49c4c07e64831f8ae2c497c4
Andreas Trepczik <andy@asustek.fritz.box>
2016-06-04 12:10:41 +0200

    spring-data-neo4j: improve configuration

--- spring-data-neo4j/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-data-neo4j/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -2,8 +2,12 @@ package com.gmail.unmacaque.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
+@EnableTransactionManagement
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
