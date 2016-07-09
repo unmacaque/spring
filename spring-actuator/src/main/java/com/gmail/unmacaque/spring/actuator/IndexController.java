de453aab6004e742d543bfa753259ee99a43f8b5
Andreas Trepczik <andy@asustek.fritz.box>
2016-07-09 19:44:16 +0200

    spring-actuator: improve Controller, cleanup configuration

--- spring-actuator/src/main/java/com/gmail/unmacaque/spring/actuator/IndexController.java
+++ spring-actuator/src/main/java/com/gmail/unmacaque/spring/actuator/IndexController.java
@@ -1,20 +1,17 @@
 package com.gmail.unmacaque.spring.actuator;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(value = "/")
+@RestController
 public class IndexController {
 
 	@Value("${application.welcome}")
 	private String welcomeText;
 
 	@RequestMapping(method = RequestMethod.GET)
-	@ResponseBody
 	public String handleIndex() {
 		return welcomeText;
 	}
