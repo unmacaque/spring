30b32b5420ff7c0f2c4913d3b88a6a425d47e03d
Andreas Trepczik <andy@asustek.fritz.box>
2016-07-09 22:11:08 +0200

    spring-boot-actuator: implement example endpoint and health indicator

---
+++ spring-boot-actuator/src/main/java/com/gmail/unmacaque/spring/actuator/endpoint/MyEndpoint.java
@@ -0,0 +1,21 @@
+package com.gmail.unmacaque.spring.actuator.endpoint;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyEndpoint extends AbstractEndpoint<Map<String, Object>> {
+
+	public MyEndpoint() {
+		super("myEndpoint", false);
+	}
+
+	@Override
+	public Map<String, Object> invoke() {
+		return Collections.singletonMap("message", "Hello World");
+	}
+
+}
