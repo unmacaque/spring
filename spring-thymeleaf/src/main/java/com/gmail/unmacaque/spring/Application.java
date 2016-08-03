f06228dc2e5396f3d897f6666332fc99a0c1f5ea
Andreas Trepczik <andy@asustek.fritz.box>
2016-08-03 19:23:03 +0200

    spring-thymeleaf: use Thymeleaf 3

--- spring-thymeleaf/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-thymeleaf/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -2,35 +2,17 @@ package com.gmail.unmacaque.spring;
 
 import java.util.Collections;
 
-import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
-import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.gmail.unmacaque.spring.thymeleaf.format.LocalDateTimeFormatter;
 
-@SpringBootApplication(exclude = { ThymeleafAutoConfiguration.class })
-public class Application extends SpringBootServletInitializer implements ApplicationContextAware {
-
-	private ApplicationContext applicationContext;
-
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
 
 	@Bean
 	public FormattingConversionServiceFactoryBean conversionService() {
@@ -42,30 +24,6 @@ public class Application extends SpringBootServletInitializer implements Applica
 	@Bean
 	public LocalDateTimeFormatter localDateTimeFormatter() {
 		return new LocalDateTimeFormatter();
-	}
-
-	@Bean
-	public ViewResolver viewResolver() {
-		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-		resolver.setTemplateEngine(templateEngine());
-		resolver.setCharacterEncoding("UTF-8");
-		return resolver;
-	}
-
-	private TemplateEngine templateEngine() {
-		SpringTemplateEngine engine = new SpringTemplateEngine();
-		engine.setEnableSpringELCompiler(true);
-		engine.setTemplateResolver(templateResolver());
-		return engine;
-	}
-
-	private ITemplateResolver templateResolver() {
-		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
-		resolver.setApplicationContext(applicationContext);
-		resolver.setPrefix("classpath:/templates/");
-		resolver.setSuffix(".html");
-		resolver.setTemplateMode(TemplateMode.HTML);
-		return resolver;
 	}
 
 	@Override
