6fd2c06ca4bdf16e577f635ec9025b2d7f6097e8
Andreas Trepczik <andy@asustek.fritz.box>
2016-08-03 19:22:50 +0200

    remove deprecated api usage

--- spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -3,7 +3,7 @@ package com.gmail.unmacaque.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
