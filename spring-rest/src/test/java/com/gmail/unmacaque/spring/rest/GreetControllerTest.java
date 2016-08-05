e72435850a057d8bce4810dd3c64e5bb6feba1d1
Andreas Trepczik <andy@asustek.fritz.box>
2016-08-05 23:20:54 +0200

    spring-rest: add back integration test

---
+++ spring-rest/src/test/java/com/gmail/unmacaque/spring/rest/GreetControllerTest.java
@@ -0,0 +1,32 @@
+package com.gmail.unmacaque.spring.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GreetControllerTest {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testGreet() throws Exception {
+		GreetResponse exchange = restTemplate.getForObject("/", GreetResponse.class);
+
+		assertThat(exchange).hasFieldOrPropertyWithValue("response", "anonymous");
+	}
+
+	@Test
+	public void testGreetWithName() throws Exception {
+		GreetResponse exchange = restTemplate.getForObject("/Spring", GreetResponse.class);
+
+		assertThat(exchange).hasFieldOrPropertyWithValue("response", "Spring");
+	}
+}
