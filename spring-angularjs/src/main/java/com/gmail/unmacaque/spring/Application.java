28bc664afd704ed39023c1d8bb9f6d570101a2d5
Andreas Trepczik <andy@asustek.fritz.box>
2016-08-06 10:06:04 +0200

    spring-angularjs: merge configuration into main application class

--- spring-angularjs/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-angularjs/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -1,12 +1,45 @@
 package com.gmail.unmacaque.spring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import com.gmail.unmacaque.spring.angularjs.HsqlTaskRepository;
+import com.gmail.unmacaque.spring.angularjs.InMemoryTaskRepository;
+import com.gmail.unmacaque.spring.angularjs.TaskRepository;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public TaskRepository inMemoryTaskRepository() {
+		return new InMemoryTaskRepository();
+	}
+
+	@Bean
+	@Primary
+	public TaskRepository taskRepository() {
+		return new HsqlTaskRepository(sessionFactory().getObject());
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setPackagesToScan("com.gmail.unmacaque.spring.angularjs");
+		return sessionFactory;
+	}
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
