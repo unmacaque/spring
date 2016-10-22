99f076e43b4ae520e4bd15f6c486598196d7bd9f
Andreas Trepczik <andy@asustek.fritz.box>
2016-10-22 09:57:32 +0200

    spring-rest: minor improvement

--- spring-rest/src/main/java/com/gmail/unmacaque/spring/rest/GreetController.java
+++ spring-rest/src/main/java/com/gmail/unmacaque/spring/rest/GreetController.java
@@ -1,20 +1,19 @@
 package com.gmail.unmacaque.spring.rest;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
-@RequestMapping
 public class GreetController {
-	@RequestMapping
+	@GetMapping
 	public GreetResponse greet() {
 		return new GreetResponse();
 	}
 
-	@RequestMapping("{name}")
+	@GetMapping("{name}")
 	public GreetResponse greet(@PathVariable("name") String name) {
 		return new GreetResponse(name);
 	}
