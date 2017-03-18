1271a2f319f09ab07618b479bda3b0b565141fe2
Andreas Trepczik <andy@asustek.fritz.box>
2017-03-18 19:32:14 +0100

    spring-security-web: remove unneeded beans

--- spring-security/spring-security-web/src/main/java/com/gmail/unmacaque/spring/config/BeanConfiguration.java
+++ spring-security/spring-security-web/src/main/java/com/gmail/unmacaque/spring/config/BeanConfiguration.java
@@ -1,23 +1,15 @@
 package com.gmail.unmacaque.spring.config;
 
 import javax.servlet.Filter;
-import javax.sql.DataSource;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 public class BeanConfiguration {
-
-	@Autowired
-	private DataSource dataSource;
 
 	@Bean
 	public Filter hiddenHttpMethodFilter() {
@@ -32,24 +24,9 @@ public class BeanConfiguration {
 	}
 
 	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-		sessionFactory.setPackagesToScan("com.gmail.unmacaque.spring.security.model");
-		return sessionFactory;
-	}
-
-	@Bean
 	public TemplateEngine templateEngine() {
 		TemplateEngine templateEngine = new TemplateEngine();
 		templateEngine.addDialect(new SpringSecurityDialect());
 		return templateEngine;
-	}
-
-	@Bean
-	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-		transactionManager.setSessionFactory(sessionFactory);
-		return transactionManager;
 	}
 }
