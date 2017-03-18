765a3d3ab88f126af94cef60267458ed2fb6f0d0
Andreas Trepczik <andy@asustek.fritz.box>
2017-03-18 18:44:26 +0100

    spring-data-rest: remove unneeded config

--- spring-data-rest/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-data-rest/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -1,27 +1,10 @@
 package com.gmail.unmacaque.spring;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class Application {
-
-	@Autowired
-	private DataSource dataSource;
-
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-		sessionFactory.setPackagesToScan("com.gmail.unmacaque.spring.data");
-		return sessionFactory;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
