fc8936a2d289b2c0abd59d576347c3ce313d937f
Andreas Trepczik <andy@asustek.fritz.box>
2017-03-26 12:24:10 +0200

    spring-webflow: messagify OrderState and show in list

--- spring-web/spring-webflow/src/main/java/com/gmail/unmacaque/spring/domain/OrderState.java
+++ spring-web/spring-webflow/src/main/java/com/gmail/unmacaque/spring/domain/OrderState.java
@@ -1,9 +1,19 @@
 package com.gmail.unmacaque.spring.domain;
 
 public enum OrderState {
-	NEW,
-	CANCELLED,
-	FULFILLED,
-	AWAITING_PAYMENT,
-	AWAITING_DELIVERY
+	NEW("orderState.new"),
+	CANCELLED("orderState.cancelled"),
+	FULFILLED("orderState.fulfilled"),
+	AWAITING_PAYMENT("orderState.waitForPayment"),
+	AWAITING_DELIVERY("orderState.waitForDelivery");
+
+	private final String value;
+
+	OrderState(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
 }
