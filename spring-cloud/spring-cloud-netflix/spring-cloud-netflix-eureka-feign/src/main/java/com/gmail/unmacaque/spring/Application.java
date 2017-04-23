898b4ebed82d516505d6bd73cbca387fad887734
Andreas Trepczik <andy@asustek.fritz.box>
2017-04-23 13:10:11 +0200

    replace @EnableEurekaClient with @EurekaDiscoveryClient

--- spring-cloud/spring-cloud-netflix/spring-cloud-netflix-eureka-feign/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-cloud/spring-cloud-netflix/spring-cloud-netflix-eureka-feign/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -2,11 +2,11 @@ package com.gmail.unmacaque.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class Application {
 	public static void main(String[] args) {
--- spring-cloud/spring-cloud-netflix/spring-cloud-netflix-eureka-service/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-cloud/spring-cloud-netflix/spring-cloud-netflix-eureka-service/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -2,10 +2,10 @@ package com.gmail.unmacaque.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
