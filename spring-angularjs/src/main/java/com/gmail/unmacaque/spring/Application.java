72d67b9ebcf70e4dc034c6a5c9e213508aad1831
Andreas Trepczik <andy@asustek.fritz.box>
2017-04-23 20:42:50 +0200

    spring-anguarjs: remove unneeded inheritance

--- spring-angularjs/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-angularjs/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -2,10 +2,9 @@ package com.gmail.unmacaque.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @SpringBootApplication
-public class Application extends RepositoryRestConfigurerAdapter {
+public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
