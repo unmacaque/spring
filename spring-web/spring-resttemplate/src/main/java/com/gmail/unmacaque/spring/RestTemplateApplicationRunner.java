784b5eec8f2455312a312f69ef0f24c4cdad2e9d
Andreas Trepczik <andy@asustek.fritz.box>
2017-04-28 20:00:16 +0200

    add basic integration test to all modules that were missing it
    this ensures that an application context can be created successfully

--- spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/RestTemplateApplicationRunner.java
+++ spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/RestTemplateApplicationRunner.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.gmail.unmacaque.spring.domain.Bundle;
 
 @Component
+@Profile("!test")
 public class RestTemplateApplicationRunner implements ApplicationRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
