f3e51dc6e5697a1598f97146080fe737319cbc15
Andreas Trepczik <andy@asustek.fritz.box>
2017-04-30 21:49:14 +0200

    spring-angular: MessageService gets data from Spring application

--- spring-angular/ng/app/message.service.ts
+++ spring-angular/ng/app/message.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
 import { Message } from './message';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class MessageService {
 
+  constructor(private http: Http) { }
+
   getMessages() : Promise<Message[]> {
-    return Promise.resolve([]);
+    return this.http.get('/api/messages')
+      .toPromise()
+      .then(response => response.json()._embedded.messages as Message[])
+      .catch(error => Promise.reject(error));
   }
 
 }
