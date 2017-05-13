8ce629d2258b3275d1bb50afc4c842d061cd3cf8
Andreas Trepczik <andy@asustek.fritz.box>
2017-05-13 20:11:31 +0200

    spring-security-ldap: create embedded LDAP server

--- spring-security/spring-security-ldap/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
+++ spring-security/spring-security-ldap/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
@@ -1,14 +1,15 @@
 package com.gmail.unmacaque.spring.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -24,16 +25,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// @formatter:on
 	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// @formatter:off
 		auth
 			.ldapAuthentication()
 				.userDnPatterns("uid={0},ou=people")
 				.groupSearchBase("ou=people")
-				.contextSource()
-					.ldif("classpath:directory.ldif")
-					.root("dc=springframework,dc=org");
+				.contextSource(contextSource());
 		// @formatter:on
+	}
+
+	@Bean
+	public DefaultSpringSecurityContextSource contextSource() {
+		return new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389"), "dc=springframework,dc=org");
 	}
 }
