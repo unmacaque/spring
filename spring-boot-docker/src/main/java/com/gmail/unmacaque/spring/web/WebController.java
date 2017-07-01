52075ab7d8a6f9030d44178d791a1cc53e99fb90
Andreas Trepczik <andy@asustek.fritz.box>
2017-07-02 00:16:51 +0200

    spring-boot-docker: add logback-logstash-encoder
    configure it so that it would log via tcp to a logstash instance
    listening on port 9600

--- spring-boot-docker/src/main/java/com/gmail/unmacaque/spring/web/WebController.java
+++ spring-boot-docker/src/main/java/com/gmail/unmacaque/spring/web/WebController.java
@@ -1,12 +1,18 @@
 package com.gmail.unmacaque.spring.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
-	@GetMapping
+
+	private static final Logger logger = LoggerFactory.getLogger(WebController.class);
+
+	@GetMapping("/")
 	public String hello() {
+		logger.info("Hello World");
 		return "Hello World";
 	}
 }
