c607897be931ca8685acb846e6152ec9f73174dc
Andreas Trepczik <andy@asustek.fritz.box>
2017-09-17 10:23:18 +0200

    spring-security-x509-client: improve configuration

--- spring-security/spring-security-x509/spring-security-x509-client/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
+++ spring-security/spring-security-x509/spring-security-x509-client/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
@@ -11,8 +11,9 @@ import java.security.cert.CertificateException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -29,29 +30,32 @@ public class SecurityConfiguration {
 
 	@Bean
 	@ConditionalOnResource(resources = "classpath:client.p12")
-	public RestTemplate restTemplate()
-			throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException,
-			KeyManagementException, UnrecoverableKeyException {
+	public RestTemplateCustomizer sslRestTemplateCustomizer() {
+		return restTemplate -> {
+			try {
+				KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+				keyStore.load(keyStoreResource.getInputStream(), "storepass".toCharArray());
 
-		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-		keyStore.load(keyStoreResource.getInputStream(), "storepass".toCharArray());
+				KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+				trustStore.load(trustStoreResource.getInputStream(), "trustpass".toCharArray());
 
-		KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-		trustStore.load(trustStoreResource.getInputStream(), "trustpass".toCharArray());
-
-		HttpClient httpClient = HttpClients.custom()
-				.setSSLContext(SSLContextBuilder.create()
-						.loadTrustMaterial(trustStore, null)
-						.loadKeyMaterial(keyStore, "storepass".toCharArray())
-						.build())
-				.build();
-		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-		return new RestTemplate(requestFactory);
+				HttpClient httpClient = HttpClients.custom()
+						.setSSLContext(SSLContextBuilder.create()
+								.loadTrustMaterial(trustStore, null)
+								.loadKeyMaterial(keyStore, "storepass".toCharArray())
+								.build())
+						.build();
+				ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+				restTemplate.setRequestFactory(requestFactory);
+			} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException | KeyManagementException
+					| UnrecoverableKeyException e) {
+				throw new IllegalStateException(e);
+			}
+		};
 	}
 
 	@Bean
-	@ConditionalOnMissingBean
-	public RestTemplate restTemplateNoSsl() {
-		return new RestTemplate();
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
