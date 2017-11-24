bd60bdf017b751a49ecf22490c626a39626cc03c
Andreas Trepczik <andy@asustek.fritz.box>
2017-11-24 19:33:35 +0100

    spring-security-web: improve test

--- spring-security/spring-security-web/src/test/java/com/gmail/unmacaque/spring/web/HelloControllerTest.java
+++ spring-security/spring-security-web/src/test/java/com/gmail/unmacaque/spring/web/HelloControllerTest.java
@@ -1,41 +1,30 @@
 package com.gmail.unmacaque.spring.web;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class HelloControllerTest {
+
 	@Autowired
-	private WebApplicationContext context;
-
 	private MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = webAppContextSetup(context)
-				.apply(springSecurity())
-				// .alwaysDo(print())
-				.build();
-	}
 
 	@Test
 	public void testIndex() throws Exception {
