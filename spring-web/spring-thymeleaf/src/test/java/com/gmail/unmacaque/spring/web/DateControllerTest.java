51fe142995a9ee03a76a3dc9a6349ef532295558
Andreas Trepczik <andy@asustek.fritz.box>
2017-11-26 13:17:54 +0100

    spring-thymeleaf: add tests

---
+++ spring-web/spring-thymeleaf/src/test/java/com/gmail/unmacaque/spring/web/DateControllerTest.java
@@ -0,0 +1,29 @@
+package com.gmail.unmacaque.spring.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(DateController.class)
+public class DateControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void test() throws Exception {
+		mvc.perform(get("/date"))
+				.andExpect(status().isOk())
+				.andExpect(model().attribute("date", notNullValue()));
+	}
+}
