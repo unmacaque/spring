a67aabcf824b7853f1ef87a27b41b6e9a748f1a3
Andreas Trepczik <andy@asustek.fritz.box>
2017-11-26 10:11:22 +0100

    spring-form-validation: move test package

--- spring-web/spring-form-validation/src/test/java/com/gmail/unmacaque/spring/web/FormControllerTest.java
+++ spring-web/spring-form-validation/src/test/java/com/gmail/unmacaque/spring/web/FormControllerTest.java
@@ -1,4 +1,4 @@
-package com.gmail.unmacaque.spring;
+package com.gmail.unmacaque.spring.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
