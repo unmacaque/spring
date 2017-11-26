e72ac4b331b7fcbe1b4dfe378170d1a9ab6155d3
Andreas Trepczik <andy@asustek.fritz.box>
2017-11-26 13:25:13 +0100

    spring-upload: add test

---
+++ spring-web/spring-upload/src/test/java/com/gmail/unmacaque/spring/web/UploadControllerTest.java
@@ -0,0 +1,48 @@
+package com.gmail.unmacaque.spring.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UploadController.class)
+public class UploadControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testGet() throws Exception {
+		mvc.perform(get("/"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testPost() throws Exception {
+		mvc.perform(fileUpload("/").file(new MockMultipartFile("aFile", "testFile", "application/octet-stream", "S".getBytes("UTF-8"))))
+				.andExpect(status().isOk())
+				.andExpect(model().attributeDoesNotExist("error"))
+				.andExpect(model().attribute("filename", "testFile"))
+				.andExpect(model().attribute("filesize", equalTo(1L)))
+				.andExpect(model().attribute("filetype", "application/octet-stream"));
+	}
+
+	@Test
+	public void testPostWithEmptyFile() throws Exception {
+		mvc.perform(fileUpload("/").file("aFile", null))
+				.andExpect(status().isOk())
+				.andExpect(model().attribute("error", "File is empty"));
+	}
+
+}
