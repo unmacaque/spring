1905299bea2c50cff8b549836df54422b11125b2
Andreas Trepczik <andy@asustek.fritz.box>
2017-11-26 15:21:27 +0100

    spring-data-rest: improve test

--- spring-data-rest/src/test/java/com/gmail/unmacaque/spring/domain/ItemRepositoryTest.java
+++ spring-data-rest/src/test/java/com/gmail/unmacaque/spring/domain/ItemRepositoryTest.java
@@ -1,97 +1,74 @@
 package com.gmail.unmacaque.spring.domain;
 
-import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
-import java.net.URI;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class ItemRepositoryTest {
 
 	@Autowired
-	private TestRestTemplate restTemplate;
+	private MockMvc mvc;
+
+	@Autowired
+	private JacksonTester<Item> json;
 
 	@Test
 	public void testGetItems() throws Exception {
-		ResponseEntity<Resources<Resource<Item>>> exchange = restTemplate.exchange(
-				"/items",
-				HttpMethod.GET,
-				null,
-				new ParameterizedTypeReference<Resources<Resource<Item>>>() {});
-
-		assertThat(exchange.getStatusCode(), equalTo(HttpStatus.OK));
-		assertThat(exchange.getBody().getContent(), hasSize(2));
+		mvc.perform(get("/items"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()", equalTo(3)));
 	}
 
 	@Test
 	public void testGetItem() throws Exception {
-		ResponseEntity<Resource<Item>> exchange = restTemplate.exchange(
-				"/items/1",
-				HttpMethod.GET,
-				null,
-				new ParameterizedTypeReference<Resource<Item>>() {});
-
-		assertThat(exchange.getStatusCode(), equalTo(HttpStatus.OK));
-		assertThat(exchange.getBody().getContent(), hasProperty("title", equalTo("CPU")));
+		mvc.perform(get("/items/1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title", equalTo("CPU")));
 	}
 
 	@Test
 	public void testPostItem() throws Exception {
-		Item item = createItem();
-
-		ResponseEntity<Void> exchange = restTemplate.postForEntity(
-				"/items",
-				item,
-				Void.class);
-
-		assertThat(exchange.getStatusCode(), equalTo(HttpStatus.CREATED));
+		mvc.perform(post("/items")
+				.content(json.write(createItem()).getJson()))
+				.andExpect(status().isCreated());
 	}
 
 	@Test
 	public void testPutItem() throws Exception {
-		Item item = createItem();
-
-		ResponseEntity<Void> exchange = restTemplate.exchange(
-				"/items/2",
-				HttpMethod.PUT,
-				RequestEntity.put(URI.create("/")).body(item),
-				Void.class);
-
-		assertThat(exchange.getStatusCode(), equalTo(HttpStatus.OK));
+		mvc.perform(put("/items/2")
+				.content(json.write(createItem()).getJson()))
+				.andExpect(status().isNoContent());
 	}
 
 	@Test
 	public void testDeleteItem() throws Exception {
-		ResponseEntity<Void> exchange = restTemplate.exchange(
-				"/items/1",
-				HttpMethod.DELETE,
-				null,
-				Void.class);
-
-		assertThat(exchange.getStatusCode(), equalTo(HttpStatus.NO_CONTENT));
+		mvc.perform(delete("/items/1"))
+				.andExpect(status().isNoContent());
 	}
 
-	private Item createItem() {
+	private static Item createItem() {
 		Item item = new Item();
 		item.setTitle("foo");
 		item.setDescription("bar");
