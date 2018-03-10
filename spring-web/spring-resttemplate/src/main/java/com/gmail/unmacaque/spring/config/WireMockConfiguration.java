38f4f9de024bd60d59a1cd553ec9ebd178564da0
Andreas Trepczik <andy@asustek.fritz.box>
2018-03-10 23:54:17 +0100

    spring-resttemplate: fix unit test

--- spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/config/WireMockConfiguration.java
+++ spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/config/WireMockConfiguration.java
@@ -2,7 +2,9 @@ package com.gmail.unmacaque.spring.config;
 
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @AutoConfigureWireMock
+@Profile("!test")
 public class WireMockConfiguration {}
