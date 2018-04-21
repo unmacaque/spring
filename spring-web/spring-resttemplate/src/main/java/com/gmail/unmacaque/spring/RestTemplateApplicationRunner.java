a31538e071bb7c6165b684ecb96d53fa0aebf526
Andreas Trepczik <andy@asustek.fritz.box>
2018-04-21 10:01:57 +0200

    spring-resttemplate: use wiremock in tests

--- spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/RestTemplateApplicationRunner.java
+++ spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/RestTemplateApplicationRunner.java
@@ -4,15 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.gmail.unmacaque.spring.domain.Bundle;
 
 @Component
-@Profile("!test")
 public class RestTemplateApplicationRunner implements ApplicationRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(RestTemplateApplicationRunner.class);
@@ -20,13 +17,9 @@ public class RestTemplateApplicationRunner implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		RestTemplate restTemplate = new RestTemplate();
-		try {
-			Bundle bundle = restTemplate.getForObject("http://localhost:8888/", Bundle.class);
-			if (bundle != null) {
-				logger.info(bundle.getContent());
-			}
-		} catch (RestClientException e) {
-			logger.error(e.toString(), e);
+		Bundle bundle = restTemplate.getForObject("http://localhost:8888/", Bundle.class);
+		if (bundle != null) {
+			logger.info(bundle.getContent());
 		}
 	}
 }
