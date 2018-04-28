96fc83f6ef1baf8f905dcd109a35d1ece588ac9d
Andreas Trepczik <andy@asustek.fritz.box>
2018-04-28 12:56:22 +0200

    spring-data-rest: make test @Transactional

--- spring-data-rest/src/test/java/com/gmail/unmacaque/spring/domain/ItemRepositoryTest.java
+++ spring-data-rest/src/test/java/com/gmail/unmacaque/spring/domain/ItemRepositoryTest.java
@@ -9,36 +9,47 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@AutoConfigureJsonTesters
+@Transactional
 public class ItemRepositoryTest {
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Autowired
+	private ObjectFactory<ObjectMapper> objectMapperFactory;
+
 	private JacksonTester<Item> json;
+
+	@Before
+	public void before() {
+		JacksonTester.initFields(this, objectMapperFactory);
+	}
 
 	@Test
 	public void testGetItems() throws Exception {
 		mvc.perform(get("/items"))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.length()", equalTo(3)));
+				.andExpect(jsonPath("$._embedded.items.length()", equalTo(3)));
 	}
 
 	@Test
