f0751cd49ae11bb5ab2ddae59aa6c52b77abb061
Andreas Trepczik <andy@asustek.fritz.box>
2018-05-13 10:53:09 +0200

    spring-security-web: improve test

--- spring-security/spring-security-web/src/test/java/com/gmail/unmacaque/spring/web/HelloControllerTest.java
+++ spring-security/spring-security-web/src/test/java/com/gmail/unmacaque/spring/web/HelloControllerTest.java
@@ -1,7 +1,6 @@
 package com.gmail.unmacaque.spring.web;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
@@ -16,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,12 +31,14 @@ public class HelloControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
+	@WithAnonymousUser
 	public void testIndex() throws Exception {
 		mockMvc.perform(get("/"))
 				.andExpect(status().isOk());
 	}
 
 	@Test
+	@WithAnonymousUser
 	public void testHello_withoutAuthentication_redirectToLogin() throws Exception {
 		mockMvc.perform(get("/hello"))
 				.andExpect(status().isFound())
@@ -43,14 +46,15 @@ public class HelloControllerTest {
 	}
 
 	@Test
+	@WithMockUser(username = "user", roles = "USER")
 	public void testHello_withRoleUser() throws Exception {
-		mockMvc.perform(get("/hello")
-				.with(user("user").roles("USER")))
+		mockMvc.perform(get("/hello"))
 				.andExpect(status().isOk())
 				.andExpect(model().attribute("username", equalTo("user")));
 	}
 
 	@Test
+	@WithAnonymousUser
 	public void testLogin_withoutAuthentication() throws Exception {
 		mockMvc.perform(get("/login"))
 				.andExpect(status().isOk());
@@ -58,34 +62,32 @@ public class HelloControllerTest {
 
 	@Test
 	public void testLogin_withRoleUser() throws Exception {
-		mockMvc.perform(formLogin()
-				.user("user")
-				.password("user"))
+		mockMvc.perform(formLogin().user("user").password("user"))
 				.andExpect(status().isFound())
 				.andExpect(authenticated().withUsername("user").withRoles("USER"))
 				.andExpect(redirectedUrl("/hello"));
 	}
 
 	@Test
+	@WithMockUser(username = "user", roles = "USER")
 	public void testLogin_withRoleUser_redirectToHello() throws Exception {
-		mockMvc.perform(get("/login")
-				.with(user("user").roles("USER")))
+		mockMvc.perform(get("/login"))
 				.andExpect(status().isOk())
 				.andExpect(forwardedUrl("/hello"));
 	}
 
 	@Test
+	@WithMockUser(username = "admin", roles = "ADMIN")
 	public void testAdmin_withRoleAdmin() throws Exception {
-		mockMvc.perform(get("/admin")
-				.with(user("admin").roles("ADMIN")))
+		mockMvc.perform(get("/admin"))
 				.andExpect(status().isOk())
 				.andExpect(model().attribute("username", equalTo("admin")));
 	}
 
 	@Test
+	@WithMockUser(username = "user", roles = "USER")
 	public void testAdmin_withRoleUser_isForbidden() throws Exception {
-		mockMvc.perform(get("/admin")
-				.with(user("somebody")))
+		mockMvc.perform(get("/admin"))
 				.andExpect(status().isForbidden());
 	}
 }
