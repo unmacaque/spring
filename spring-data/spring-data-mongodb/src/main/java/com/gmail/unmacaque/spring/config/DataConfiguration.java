3c26edbad6bb261c61e640f4c2f046ead2f55b96
Andreas Trepczik <andy@asustek.fritz.box>
2018-10-27 19:03:51 +0200

    add spring-data-mongodb project

---
+++ spring-data/spring-data-mongodb/src/main/java/com/gmail/unmacaque/spring/config/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.gmail.unmacaque.spring.config;
+
+import com.gmail.unmacaque.spring.domain.Quote;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.core.CollectionOptions;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.repository.init.Jackson2ResourceReader;
+
+import java.util.ArrayList;
+
+@Configuration
+public class DataConfiguration implements ApplicationListener<ApplicationReadyEvent> {
+
+	private final ClassPathResource resource = new ClassPathResource("data.json");
+
+	private final ReactiveMongoTemplate mongoTemplate;
+
+	public DataConfiguration(ReactiveMongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		mongoTemplate.createCollection(Quote.class, CollectionOptions.empty().capped().size(1000L)).subscribe();
+		// work around missing RepositoryPopulator support for reactive repositories
+		Jackson2ResourceReader reader = new Jackson2ResourceReader();
+		try {
+			@SuppressWarnings("unchecked")
+			ArrayList<Quote> quotes = (ArrayList<Quote>) reader.readFrom(resource, null);
+			mongoTemplate.insertAll(quotes).subscribe();
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
---
+++ spring-data/spring-data-mongodb/src/main/java/com/gmail/unmacaque/spring/webflux/RouteHandler.java
@@ -0,0 +1,36 @@
+package com.gmail.unmacaque.spring.webflux;
+
+import com.gmail.unmacaque.spring.domain.Quote;
+import com.gmail.unmacaque.spring.domain.QuoteRepository;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class RouteHandler {
+
+	private final QuoteRepository quoteRepository;
+
+	public RouteHandler(QuoteRepository quoteRepository) {
+		this.quoteRepository = quoteRepository;
+	}
+
+	public Mono<ServerResponse> getQuote(ServerRequest request) {
+		String id = request.pathVariables().get("id");
+		return ServerResponse
+				.ok()
+				.body(quoteRepository.findById(id), Quote.class);
+	}
+
+	public Mono<ServerResponse> getQuotes(ServerRequest request) {
+		return ServerResponse
+				.ok()
+				.body(quoteRepository.findAll(), Quote.class);
+	}
+
+	public Mono<ServerResponse> postQuote(ServerRequest request) {
+		Mono<Quote> quote = request.bodyToMono(Quote.class);
+		return ServerResponse
+				.ok()
+				.body(quoteRepository.saveAll(quote), Quote.class);
+	}
+}
