f3ffdefd8a8662b7fc4ad8a2f723fa886ae7e2ce
Andreas Trepczik <andy@asustek.fritz.box>
2018-10-31 23:12:07 +0100

    update spring-boot to 2.1.0.RELEASE

--- spring-security/spring-security-2fa/src/main/java/com/gmail/unmacaque/spring/config/BeanConfiguration.java
+++ spring-security/spring-security-2fa/src/main/java/com/gmail/unmacaque/spring/config/BeanConfiguration.java
@@ -2,7 +2,7 @@ package com.gmail.unmacaque.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
 public class BeanConfiguration {
