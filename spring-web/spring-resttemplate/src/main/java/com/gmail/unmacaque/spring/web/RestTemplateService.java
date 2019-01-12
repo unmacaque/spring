3565213bc37d2acd5d56b1201f49d490a4839004
Andreas Trepczik <andy@asustek.fritz.box>
2019-01-12 13:14:26 +0100

    spring-resttemplate: replace wiremock with MockRestServiceServer

---
+++ spring-web/spring-resttemplate/src/main/java/com/gmail/unmacaque/spring/web/RestTemplateService.java
@@ -0,0 +1,28 @@
+package com.gmail.unmacaque.spring.web;
+
+import com.gmail.unmacaque.spring.domain.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestTemplateService {
+
+	private static final Logger logger = LoggerFactory.getLogger(RestTemplateService.class);
+
+	private final RestTemplate restTemplate;
+
+	public RestTemplateService(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
+
+	public String doCall() {
+		Bundle bundle = restTemplate.getForObject("http://localhost:8888/", Bundle.class);
+		if (bundle != null) {
+			logger.info(bundle.getContent());
+		}
+		return bundle.toString();
+	}
+}
--- spring-web/spring-resttemplate/src/test/java/com/gmail/unmacaque/spring/ApplicationTest.java
+++ spring-web/spring-resttemplate/src/test/java/com/gmail/unmacaque/spring/ApplicationTest.java
@@ -1,17 +1,38 @@
 package com.gmail.unmacaque.spring;
 
+import com.gmail.unmacaque.spring.web.RestTemplateService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+import static org.springframework.test.web.client.ExpectedCount.once;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.anything;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
-@AutoConfigureWireMock(port = 8888)
+@RestClientTest(RestTemplateService.class)
 public class ApplicationTest {
+
+	@Autowired
+	private RestTemplateService service;
+
+	@Autowired
+	private MockRestServiceServer server;
+
 	@Test
-	public void test() {}
+	public void test() throws Exception {
+		server.expect(once(), anything()).andRespond(
+				withSuccess()
+						.body("{\"content\":\"Hello World\"}")
+						.contentType(MediaType.APPLICATION_JSON)
+		);
+
+		service.doCall();
+		server.verify();
+	}
+
 }
