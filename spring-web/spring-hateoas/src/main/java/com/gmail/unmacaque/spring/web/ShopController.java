a8da5e87c9d5d2dc3fc2e4b4bee0c5b69a3f323e
Andreas Trepczik <andy@asustek.fritz.box>
2019-02-15 19:16:50 +0100

    update spring-boot to 2.1.3.RELEASE

--- spring-web/spring-hateoas/src/main/java/com/gmail/unmacaque/spring/web/ShopController.java
+++ spring-web/spring-hateoas/src/main/java/com/gmail/unmacaque/spring/web/ShopController.java
@@ -1,82 +1,70 @@
 package com.gmail.unmacaque.spring.web;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-
-import org.springframework.hateoas.EntityLinks;
+import com.gmail.unmacaque.spring.domain.Item;
+import com.gmail.unmacaque.spring.domain.Shop;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.gmail.unmacaque.spring.domain.Item;
-import com.gmail.unmacaque.spring.domain.Shop;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
-@RequestMapping(value = "/", produces = { MediaType.APPLICATION_JSON_VALUE })
 @ExposesResourceFor(Item.class)
 public class ShopController {
 
 	private final Shop shop;
 
-	private final EntityLinks entityLinks;
-
-	public ShopController(Shop shop, EntityLinks entityLinks) {
+	public ShopController(Shop shop) {
 		this.shop = shop;
-		this.entityLinks = entityLinks;
 	}
 
 	@GetMapping
 	public Resources<Resource<Item>> getAllItems() {
 		List<Resource<Item>> itemResources = new ArrayList<>();
 
-		for (Item item : shop.getItems()) {
-			itemResources.add(buildItemResource(item));
-		}
+		shop.getItems().forEach(item -> itemResources.add(buildItemResource(item)));
 
 		Resources<Resource<Item>> resources = new Resources<>(itemResources);
-		resources.add(entityLinks.linkToCollectionResource(Item.class));
+		resources.add(linkTo(ShopController.class).withSelfRel());
 
 		return resources;
 	}
 
-	@GetMapping(value = "/{itemId}")
+	@GetMapping("/{itemId}")
 	public Resource<Item> getItem(@PathVariable int itemId) {
 		Item item = shop.findItemById(itemId).orElseThrow(NoSuchElementException::new);
 
 		Resource<Item> resource = new Resource<>(item);
-		resource.add(entityLinks.linkToSingleResource(Item.class, itemId));
+		resource.add(linkTo(ShopController.class).slash(item).withSelfRel());
 
 		return resource;
 	}
 
-	@PostMapping(value = "/{itemId}/order")
+	@PostMapping("/{itemId}/order")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Resource<Item> orderItem(@PathVariable int itemId) {
 		Item item = shop.findItemById(itemId).orElseThrow(NoSuchElementException::new);
 
 		Resource<Item> resource = new Resource<>(item);
-		resource.add(entityLinks.linkToSingleResource(Item.class, itemId));
+		resource.add(linkTo(ShopController.class).slash(item).withSelfRel());
 
 		return resource;
 	}
 
 	private Resource<Item> buildItemResource(Item item) {
 		Resource<Item> resource = new Resource<>(item);
-		resource.add(entityLinks.linkToSingleResource(Item.class, item.getItemId()));
+		resource.add(linkTo(ShopController.class).slash(item).withSelfRel());
 		return resource;
 	}
 
 	@ExceptionHandler(NoSuchElementException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public void handleNoSuchElementException() {}
+	public void handleNoSuchElementException() {
+	}
 }
