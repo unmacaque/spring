03c4238b8975166ea8b789115fc997364973dee9
Andreas Trepczik <andy@asustek.fritz.box>
2019-06-14 18:21:22 +0200

    spring-angular: update to Angular 8

--- spring-angular/ng/e2e/src/app.e2e-spec.ts
+++ spring-angular/ng/e2e/src/app.e2e-spec.ts
@@ -11,4 +11,12 @@ describe('spring-angular App', () => {
     page.navigateTo();
     expect(page.getTitle()).toEqual('Spring Boot &amp; Angular');
   });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
 });
