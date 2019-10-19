a7baecb3a8a3d2adc652e3310c35f80105b25368
Andreas Trepczik <andy@asrock.fritz.box>
2019-10-19 11:14:19 +0200

    update spring-boot to 2.2.0.RELEASE

--- spring-web/spring-hateoas/src/main/java/com/gmail/unmacaque/spring/web/ShopController.java
+++ spring-web/spring-hateoas/src/main/java/com/gmail/unmacaque/spring/web/ShopController.java
@@ -2,16 +2,16 @@ package com.gmail.unmacaque.spring.web;
 
 import com.gmail.unmacaque.spring.domain.Item;
 import com.gmail.unmacaque.spring.domain.Shop;
-import org.springframework.hateoas.ExposesResourceFor;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @ExposesResourceFor(Item.class)
@@ -24,42 +24,42 @@ public class ShopController {
 	}
 
 	@GetMapping
-	public Resources<Resource<Item>> getAllItems() {
-		var itemResources = new ArrayList<Resource<Item>>();
+	public CollectionModel<EntityModel<Item>> getAllItems() {
+		var itemCollectionModel = new ArrayList<EntityModel<Item>>();
 
-		shop.getItems().forEach(item -> itemResources.add(buildItemResource(item)));
+		shop.getItems().forEach(item -> itemCollectionModel.add(buildItemEntityModel(item)));
 
-		var resources = new Resources<>(itemResources);
-		resources.add(linkTo(ShopController.class).withSelfRel());
+		var CollectionModel = new CollectionModel<>(itemCollectionModel);
+		CollectionModel.add(linkTo(ShopController.class).withSelfRel());
 
-		return resources;
+		return CollectionModel;
 	}
 
 	@GetMapping("/{itemId}")
-	public Resource<Item> getItem(@PathVariable int itemId) {
+	public EntityModel<Item> getItem(@PathVariable int itemId) {
 		var item = shop.findItemById(itemId).orElseThrow(NoSuchElementException::new);
 
-		var resource = new Resource<>(item);
-		resource.add(linkTo(ShopController.class).slash(item).withSelfRel());
+		var EntityModel = new EntityModel<>(item);
+		EntityModel.add(linkTo(ShopController.class).slash(item).withSelfRel());
 
-		return resource;
+		return EntityModel;
 	}
 
 	@PostMapping("/{itemId}/order")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Resource<Item> orderItem(@PathVariable int itemId) {
+	public EntityModel<Item> orderItem(@PathVariable int itemId) {
 		var item = shop.findItemById(itemId).orElseThrow(NoSuchElementException::new);
 
-		var resource = new Resource<>(item);
-		resource.add(linkTo(ShopController.class).slash(item).withSelfRel());
+		var EntityModel = new EntityModel<>(item);
+		EntityModel.add(linkTo(ShopController.class).slash(item).withSelfRel());
 
-		return resource;
+		return EntityModel;
 	}
 
-	private Resource<Item> buildItemResource(Item item) {
-		var resource = new Resource<>(item);
-		resource.add(linkTo(ShopController.class).slash(item).withSelfRel());
-		return resource;
+	private EntityModel<Item> buildItemEntityModel(Item item) {
+		var EntityModel = new EntityModel<>(item);
+		EntityModel.add(linkTo(ShopController.class).slash(item).withSelfRel());
+		return EntityModel;
 	}
 
 	@ExceptionHandler(NoSuchElementException.class)
