26f801977f3068c0e0f5764799562eea767cdcfd
Andreas Trepczik <andy@asrock.fritz.box>
2019-10-19 09:07:27 +0200

    fix wrong annotation

--- spring-boot/src/main/java/com/gmail/unmacaque/spring/autoconfigure/MyBeanAutoConfiguration.java
+++ spring-boot/src/main/java/com/gmail/unmacaque/spring/autoconfigure/MyBeanAutoConfiguration.java
@@ -5,11 +5,11 @@ import com.gmail.unmacaque.spring.domain.MyBeanImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties
+@Configuration
 @ConditionalOnClass(MyBean.class)
 @EnableConfigurationProperties(MyBeanProperties.class)
 public class MyBeanAutoConfiguration {
