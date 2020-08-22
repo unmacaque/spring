4d63a050f20ca122b02675aec52f73ecfbd62b87
Andreas Trepczik <andy@asrock.fritz.box>
2020-08-22 15:06:33 +0200

    add spring-security-oauth2-webflux

---
+++ spring-security/spring-security-oauth2/spring-security-oauth2-webflux/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.gmail.unmacaque.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http
+				.authorizeExchange(authorizeExchange ->
+						authorizeExchange
+								.anyExchange().authenticated())
+				.oauth2Login(withDefaults())
+				.build();
+	}
+
+}
---
+++ spring-security/spring-security-oauth2/spring-security-oauth2-webflux/src/test/java/com/gmail/unmacaque/spring/web/WebControllerTest.java
@@ -0,0 +1,29 @@
+package com.gmail.unmacaque.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockOidcLogin;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class WebControllerTest {
+
+	@Autowired
+	private WebTestClient client;
+
+	@Test
+	void testIndex() throws Exception {
+		client
+				.mutateWith(mockOidcLogin())
+				.get()
+				.uri("/")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("user");
+	}
+
+}
