7382b7205b87e49b112d45a7687c640b8611e9eb
Andreas Trepczik <andy@asrock.fritz.box>
2021-06-16 08:37:56 +0200

    add spring-security-oauth2-resource-server

---
+++ spring-security/spring-security-oauth2/spring-security-oauth2-resource-server/src/main/java/com/gmail/unmacaque/spring/web/WebController.java
@@ -0,0 +1,15 @@
+package com.gmail.unmacaque.spring.web;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	@GetMapping("/")
+	public Object index(JwtAuthenticationToken token) {
+		return token.getName();
+	}
+
+}
