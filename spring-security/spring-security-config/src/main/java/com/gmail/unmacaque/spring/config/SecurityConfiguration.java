7aaa2e1e5f75ffe10b7e60cb3a745fe869580319
Andreas Trepczik <andy@asrock.fritz.box>
2021-06-17 16:44:17 +0200

    spring-security-config: rework with multiple configurations

--- spring-security/spring-security-config/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
+++ spring-security/spring-security-config/src/main/java/com/gmail/unmacaque/spring/config/SecurityConfiguration.java
@@ -2,49 +2,73 @@ package com.gmail.unmacaque.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-	@Autowired
-	private DataSource dataSource;
+	@Configuration
+	@Order(1)
+	public static class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers("/h2-console/**");
+		@Autowired
+		private DataSource dataSource;
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.antMatcher("/api/**")
+					.authorizeRequests(authorizeRequests ->
+							authorizeRequests
+									.anyRequest().hasRole("ADMIN")
+					)
+					.httpBasic(withDefaults());
+		}
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth
+					.jdbcAuthentication()
+					.dataSource(dataSource)
+					.passwordEncoder(new BCryptPasswordEncoder());
+		}
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests(authorizeRequests ->
-						authorizeRequests
-								.antMatchers("/api").hasRole("ADMIN")
-								.anyRequest().hasRole("USER")
-				)
-				.formLogin(withDefaults())
-				.httpBasic(withDefaults());
-	}
+	@Configuration
+	public static class UserSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// @formatter:off
-		auth
-			.jdbcAuthentication()
-				.dataSource(dataSource)
-				.passwordEncoder(new BCryptPasswordEncoder())
-		.and()
-			.inMemoryAuthentication()
-				.withUser("user").password("user").roles("USER");
-		// @formatter:on
+		@Override
+		public void configure(WebSecurity web) {
+			web.ignoring().antMatchers("/h2-console/**");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests(authorizeRequests ->
+							authorizeRequests
+									.anyRequest().authenticated()
+					)
+					.formLogin(withDefaults());
+		}
+
+		@SuppressWarnings("deprecation")
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth
+					.inMemoryAuthentication()
+					.passwordEncoder(NoOpPasswordEncoder.getInstance())
+					.withUser("user").password("user").roles("USER");
+		}
 	}
 }
