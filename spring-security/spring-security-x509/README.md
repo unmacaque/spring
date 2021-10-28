a13ee0f29fc1c1d8a8001c9f559ee6327897d6a1
Andreas Trepczik <andy@asrock.fritz.box>
2021-10-28 09:12:35 +0200

    spring-security-x509: convert trust store to pkcs12

--- spring-security/spring-security-x509/README.md
+++ spring-security/spring-security-x509/README.md
@@ -1,6 +1,10 @@
 # spring-security-x509
 
-Mutual SSL authentication with Spring Security.
+Mutual TLS authentication with Spring Security.
 
 1. Run `./openssl.sh`
 1. Start `spring-security-x509`
+
+Example authenticated request using _cURL_:
+
+    curl --cert client.crt --key client.key --cacert rootca.crt https://localhost:8443
--- spring-security/spring-security-x509/openssl.sh
+++ spring-security/spring-security-x509/openssl.sh
@@ -8,11 +8,13 @@ cd .tmp-ssl
 
 openssl req -new -x509 -days 3650 -extensions v3_ca -subj "/C=DE/ST=Baden-Wuerttemberg/O=Spring Samples by unmacaque/OU=spring-security-x509/CN=root CA/" -keyout rootca.key -out rootca.crt -nodes
 keytool -import -noprompt -file rootca.crt -storepass trustpass -alias "root CA" -keystore trust.jks
+keytool -importkeystore -noprompt -srckeystore trust.jks -srcstoretype JKS -srcstorepass trustpass -destkeystore trust.p12 -deststoretype PKCS12 -deststorepass trustpass
+cp -fv trust.p12 ../src/main/resources
 
 for identity in client server; do
   openssl req -new -subj "/C=DE/ST=Baden-Wuerttemberg/O=Spring Samples by unmacaque/OU=spring-security-x509/CN=localhost/" -keyout ${identity}.key -out ${identity}.csr -nodes
   openssl x509 -req -days 3650 -CAkey rootca.key -CA rootca.crt -CAcreateserial -in ${identity}.csr -out ${identity}.crt
   openssl pkcs12 -export -passout pass:storepass -inkey ${identity}.key -in ${identity}.crt -name ${identity} -out ${identity}.p12
 
-  cp -fv ${identity}.p12 trust.jks ../src/main/resources
+  cp -fv ${identity}.p12 ../src/main/resources
 done
