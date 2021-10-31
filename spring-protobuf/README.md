c604fd9717952bfaf1bdb2bc82adc2d8cec9104a
Andreas Trepczik <andy@asrock.fritz.box>
2021-10-31 18:00:13 +0100

    add spring-protobuf

---
+++ spring-protobuf/README.md
@@ -0,0 +1,6 @@
+# spring-protobuf
+
+A sample project using demonstrating the support of Spring Framework for Google Protocol Buffers in a Spring MVC application.
+
+- <https://developers.google.com/protocol-buffers/docs/proto3>
+- <https://developers.google.com/protocol-buffers/docs/reference/java-generated>
---
+++ spring-protobuf/src/main/java/com/gmail/unmacaque/spring/web/WebController.java
@@ -0,0 +1,26 @@
+package com.gmail.unmacaque.spring.web;
+
+import com.gmail.unmacaque.spring.domain.hello.Greeting;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Greeting getHello() {
+		return Greeting
+				.newBuilder()
+				.setMessage("Hello World")
+				.setDayOfWeek(Greeting.DayOfWeek.FRIDAY)
+				.build();
+	}
+
+	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String postHello(@RequestBody Greeting greeting) {
+		return greeting.getMessage();
+	}
+}
