828f219ba50ab6ae81529ab2e3fd41b8851313f1
Andreas Trepczik <andy@asrock.fritz.box>
2021-11-12 16:29:16 +0100

    fix unused imports

--- spring-grpc/spring-grpc-server/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-grpc/spring-grpc-server/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -1,14 +1,7 @@
 package com.gmail.unmacaque.spring;
 
-import com.gmail.unmacaque.spring.domain.ForecastService;
-import io.grpc.ServerBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
