db6f6a21c4a43a892c16175815df10de979ca976
Andreas Trepczik <andy@asrock.fritz.box>
2021-12-03 16:00:03 +0100

    spring-boot-docker: populate info, health endpoints

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("com.github.node-gradle:gradle-node-plugin:3.1.0")
     implementation("com.palantir.gradle.docker:gradle-docker:0.30.0")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
+    implementation("com.gorylenko.gradle-git-properties:gradle-git-properties:2.3.2")
     implementation("org.asciidoctor:asciidoctor-gradle-jvm:3.3.2")
     implementation("org.springdoc:springdoc-openapi-gradle-plugin:1.3.3")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins:gradle-processes:0.5.0")
