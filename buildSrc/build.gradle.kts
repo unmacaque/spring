7c58a758c6b588eca7569c6efbf71c4fdbc779e5
Andreas Trepczik <andy@asrock.fritz.box>
2022-03-01 15:04:46 +0100

    spring-native: update, fix nativeCompile task

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,8 +16,8 @@ dependencies {
     implementation("org.asciidoctor:asciidoctor-gradle-jvm:3.3.2")
     implementation("org.springdoc:springdoc-openapi-gradle-plugin:1.3.3")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins:gradle-processes:0.5.0")
-    implementation("org.graalvm.buildtools:native-gradle-plugin:0.9.9")
-    implementation("org.springframework.experimental:spring-aot-gradle-plugin:0.11.2")
+    implementation("org.graalvm.buildtools:native-gradle-plugin:0.9.10")
+    implementation("org.springframework.experimental:spring-aot-gradle-plugin:0.11.3")
 }
 
 repositories {
--- spring-native/README.md
+++ spring-native/README.md
@@ -1,5 +1,5 @@
 # spring-native
 
-1. Install [GraalVM with native-image support](https://graalvm.github.io/native-build-tools/latest/graalvm-setup.html)
+1. Install [GraalVM with native-image support](https://graalvm.github.io/native-build-tools/latest/graalvm-setup.html) (_last verified with GraalVM 22.0_)
 2. Run the task `nativeCompile` (consumes a lot of memory)
 3. Run the executable `/build/native/nativeCompile/spring-native`
