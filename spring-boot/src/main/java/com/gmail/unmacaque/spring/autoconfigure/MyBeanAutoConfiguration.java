b0e94f96ee69b6519801f454927f4835a756c644
Andreas Trepczik <andy@asrock.fritz.box>
2022-05-20 17:43:21 +0200

    spring-boot: adapt to new autoconfiguration style

--- spring-boot/src/main/java/com/gmail/unmacaque/spring/autoconfigure/MyBeanAutoConfiguration.java
+++ spring-boot/src/main/java/com/gmail/unmacaque/spring/autoconfigure/MyBeanAutoConfiguration.java
@@ -3,13 +3,13 @@ package com.gmail.unmacaque.spring.autoconfigure;
 import com.gmail.unmacaque.spring.domain.MyBean;
 import com.gmail.unmacaque.spring.domain.MyBeanImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnClass(MyBean.class)
 @EnableConfigurationProperties(MyBeanProperties.class)
 public class MyBeanAutoConfiguration {
