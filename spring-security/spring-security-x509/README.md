1cb826fa4b529000654c61605a1fe94fc325f486
Andreas Trepczik <andy@asrock.fritz.box>
2022-05-20 17:44:09 +0200

    spring-security-x509: use certificate properties

--- spring-security/spring-security-x509/README.md
+++ spring-security/spring-security-x509/README.md
@@ -7,4 +7,4 @@ Mutual TLS authentication with Spring Security.
 
 Example authenticated request using _cURL_:
 
-    curl --cert client.crt --key client.key --cacert rootca.crt https://localhost:8443
+    curl --cert client.pem --key client.key --cacert trust.pem https://localhost:8443
--- spring-security/spring-security-x509/openssl.sh
+++ spring-security/spring-security-x509/openssl.sh
@@ -6,15 +6,18 @@ rm -rfv .tmp-ssl
 mkdir -v .tmp-ssl
 cd .tmp-ssl
 
-openssl req -new -x509 -days 3650 -extensions v3_ca -subj "/C=DE/ST=Baden-Wuerttemberg/O=Spring Samples by unmacaque/OU=spring-security-x509/CN=root CA/" -keyout rootca.key -out rootca.crt -nodes
-keytool -import -noprompt -file rootca.crt -storepass trustpass -alias "root CA" -keystore trust.jks
-keytool -importkeystore -noprompt -srckeystore trust.jks -srcstoretype JKS -srcstorepass trustpass -destkeystore trust.p12 -deststoretype PKCS12 -deststorepass trustpass
-cp -fv trust.p12 ../src/main/resources
+openssl genrsa -out trust.key
+openssl req -new -x509 -days 3650 -extensions v3_ca \
+    -subj "/C=DE/ST=Baden-Wuerttemberg/O=Spring Samples by unmacaque/OU=spring-security-x509/CN=root CA/" \
+    -key trust.key -out trust.pem -nodes
+cp -fv trust.* ../src/main/resources
 
 for identity in client server; do
-  openssl req -new -subj "/C=DE/ST=Baden-Wuerttemberg/O=Spring Samples by unmacaque/OU=spring-security-x509/CN=localhost/" -keyout ${identity}.key -out ${identity}.csr -nodes
-  openssl x509 -req -days 3650 -CAkey rootca.key -CA rootca.crt -CAcreateserial -in ${identity}.csr -out ${identity}.crt
-  openssl pkcs12 -export -passout pass:storepass -inkey ${identity}.key -in ${identity}.crt -name ${identity} -out ${identity}.p12
-
-  cp -fv ${identity}.p12 ../src/main/resources
+    openssl genrsa -out ${identity}.key
+    openssl req -new \
+        -subj "/C=DE/ST=Baden-Wuerttemberg/O=Spring Samples by unmacaque/OU=spring-security-x509/CN=localhost/" \
+        -key ${identity}.key -out ${identity}.csr -nodes
+    openssl x509 -req -days 3650 -CAkey trust.key -CA trust.pem -CAcreateserial -in ${identity}.csr -out ${identity}.crt
+    cat ${identity}.crt trust.pem > ${identity}.pem
+    cp -fv ${identity}.pem ${identity}.key ../src/main/resources
 done
