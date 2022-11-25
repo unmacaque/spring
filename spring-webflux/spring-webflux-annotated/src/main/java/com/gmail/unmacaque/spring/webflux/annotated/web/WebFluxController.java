07c06bb8e9e115d15105a0074d402f4fb6b67175
Andreas Trepczik <andy@asrock.fritz.box>
2022-11-25 16:21:15 +0100

    spring-webflux-annotated: add server-sent events sample

--- spring-webflux/spring-webflux-annotated/src/main/java/com/gmail/unmacaque/spring/webflux/annotated/web/WebFluxController.java
+++ spring-webflux/spring-webflux-annotated/src/main/java/com/gmail/unmacaque/spring/webflux/annotated/web/WebFluxController.java
@@ -1,11 +1,13 @@
 package com.gmail.unmacaque.spring.webflux.annotated.web;
 
 import com.gmail.unmacaque.spring.webflux.annotated.domain.Greeting;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.Instant;
 
 @RestController
@@ -17,9 +19,20 @@ public class WebFluxController {
 	}
 
 	@GetMapping("/flux")
-	public Flux<Greeting> helloFlux() {
+	public Flux<Greeting> flux() {
 		return Flux
 				.<Greeting>generate(s -> s.next(new Greeting("Hello World", Instant.now())))
 				.take(10);
+	}
+
+	@GetMapping("/events")
+	public Flux<ServerSentEvent<Greeting>> events() {
+		return Flux
+				.interval(Duration.ZERO, Duration.ofSeconds(1))
+				.map(aLong -> ServerSentEvent.<Greeting>builder()
+						.id(String.valueOf(aLong))
+						.event("message")
+						.data(new Greeting("Hello World", Instant.now()))
+						.build());
 	}
 }
--- spring-webflux/spring-webflux-annotated/src/test/java/com/gmail/unmacaque/spring/webflux/annotated/web/WebFluxControllerTest.java
+++ spring-webflux/spring-webflux-annotated/src/test/java/com/gmail/unmacaque/spring/webflux/annotated/web/WebFluxControllerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.test.StepVerifier;
 
@@ -49,6 +51,23 @@ class WebFluxControllerTest {
 				.thenAwait(Duration.ofSeconds(9))
 				.expectNextCount(9)
 				.verifyComplete();
+	}
+
+	@Test
+	void testEvents() {
+		final Supplier<? extends Publisher<? extends ServerSentEvent<Greeting>>> scenarioSupplier = () -> webTestClient
+				.get()
+				.uri("/events")
+				.accept(MediaType.TEXT_EVENT_STREAM)
+				.exchange()
+				.expectStatus().isOk()
+				.returnResult(new ParameterizedTypeReference<ServerSentEvent<Greeting>>() {}).getResponseBody();
+
+		StepVerifier.withVirtualTime(scenarioSupplier)
+				.expectSubscription()
+				.assertNext(event -> assertThat(event.data()).isNotNull().satisfies(this::assertGreeting))
+				.thenCancel()
+				.verify();
 	}
 
 	private void assertGreeting(Greeting greeting) {
