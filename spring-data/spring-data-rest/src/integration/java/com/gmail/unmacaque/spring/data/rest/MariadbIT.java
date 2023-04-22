1d39e58d2b8a2d0177f799d41777d57f9efdc4f7
Andreas Trepczik <andy@asrock.fritz.box>
2023-04-22 15:49:48 +0200

    make use of spring-boot-testcontainers

--- spring-data/spring-data-rest/src/integration/java/com/gmail/unmacaque/spring/data/rest/MariadbIT.java
+++ spring-data/spring-data-rest/src/integration/java/com/gmail/unmacaque/spring/data/rest/MariadbIT.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MariaDBContainer;
@@ -24,7 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MariadbIT {
 
 	@Container
-	private static final MariaDBContainer<?> mariadbContrainer = new MariaDBContainer<>("mariadb:10");
+	@ServiceConnection
+	static final MariaDBContainer<?> mariadbContrainer = new MariaDBContainer<>("mariadb:10");
 
 	@Autowired
 	private MockMvc mvc;
