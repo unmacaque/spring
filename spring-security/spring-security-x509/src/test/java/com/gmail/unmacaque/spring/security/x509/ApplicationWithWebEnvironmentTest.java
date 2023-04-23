54cf1f236c1a4018058b82f733d6b3b08c75c36d
Andreas Trepczik <andy@asrock.fritz.box>
2023-04-23 20:14:26 +0200

    make use of ssl bundles

---
+++ spring-security/spring-security-x509/src/test/java/com/gmail/unmacaque/spring/security/x509/ApplicationWithWebEnvironmentTest.java
@@ -0,0 +1,40 @@
+package com.gmail.unmacaque.spring.security.x509;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class ApplicationWithWebEnvironmentTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void testGet() {
+		final var entity = restTemplate.getForEntity("/", String.class);
+
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@TestConfiguration(proxyBeanMethods = false)
+	static class RestTemplateBuilderConfiguration {
+
+		@Bean
+		public RestTemplateBuilder restTemplateBuilder(SslBundles sslBundles) {
+			final var bundle = sslBundles.getBundle("test");
+			return new RestTemplateBuilder().setSslBundle(bundle);
+		}
+
+	}
+}
