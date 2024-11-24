db535d18b2271b66a1fd0e98d0808d49e4a4c0ad
Andreas Trepczik <andy@fritz.box>
2024-11-24 16:47:41 +0100

    enable ssl info contributor and health indicator

--- projects/security/x509/src/main/java/com/gmail/unmacaque/spring/security/x509/config/SecurityConfiguration.java
+++ projects/security/x509/src/main/java/com/gmail/unmacaque/spring/security/x509/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.gmail.unmacaque.spring.security.x509.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
 	@Bean
+	@Order(1)
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 				.authorizeHttpRequests(requests ->
@@ -21,6 +24,18 @@ public class SecurityConfiguration {
 						x509
 								.subjectPrincipalRegex("CN=(.*?)(?:,|$)")
 								.userDetailsService(userDetailsService())
+				)
+				.build();
+	}
+
+	@Bean
+	@Order(0)
+	public SecurityFilterChain managementSecurityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.securityMatcher(EndpointRequest.toAnyEndpoint())
+				.authorizeHttpRequests(requests ->
+						requests
+								.anyRequest().permitAll()
 				)
 				.build();
 	}
--- projects/security/x509/src/test/java/com/gmail/unmacaque/spring/security/x509/ApplicationWithWebEnvironmentTest.java
+++ projects/security/x509/src/test/java/com/gmail/unmacaque/spring/security/x509/ApplicationWithWebEnvironmentTest.java
@@ -3,28 +3,55 @@ package com.gmail.unmacaque.spring.security.x509;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.json.BasicJsonTester;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalManagementPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureJsonTesters
 @ActiveProfiles("test")
 class ApplicationWithWebEnvironmentTest {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	@Autowired
+	private BasicJsonTester json;
+
+	@LocalManagementPort
+	private int managementPort;
+
 	@Test
 	void testGet() {
 		final var entity = restTemplate.getForEntity("/", String.class);
 
 		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	void testActuatorInfo() {
+		final var info = RestClient.builder()
+				.baseUrl("http://localhost:{port}")
+				.defaultUriVariables(Map.of("port", managementPort))
+				.build()
+				.get()
+				.uri("/actuator/info")
+				.retrieve()
+				.body(String.class);
+
+		assertThat(json.from(info)).hasJsonPathStringValue("$.ssl.bundles[0].name", "server");
 	}
 
 	@TestConfiguration(proxyBeanMethods = false)
