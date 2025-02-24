6686d16e9785443c431ec1045ff4095498d3a64f
Andreas Trepczik <andy@fritz.box>
2025-02-24 16:51:53 +0100

    spring-ai: add tools example

--- projects/ai/src/main/java/com/gmail/unmacaque/spring/ai/web/AiController.java
+++ projects/ai/src/main/java/com/gmail/unmacaque/spring/ai/web/AiController.java
@@ -1,5 +1,7 @@
 package com.gmail.unmacaque.spring.ai.web;
 
+import com.gmail.unmacaque.spring.ai.domain.DateTimeTools;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,5 +19,14 @@ public class AiController {
 	@GetMapping("/")
 	public String generate(@RequestParam(value = "message", defaultValue = "Say Hello World") String message) {
 		return aiModel.call(message);
+	}
+
+	@GetMapping("/time")
+	public String time(@RequestParam(value = "message", defaultValue = "What is the current time?") String message) {
+		return ChatClient.create(aiModel)
+				.prompt(message)
+				.tools(new DateTimeTools())
+				.call()
+				.content();
 	}
 }
