bc7be403ad406cd69852aba97c9899be45e31f79
Andreas Trepczik <andy@fritz.box>
2025-04-13 21:30:44 +0200

    fix chat test

--- projects/ai/chat/src/test/java/com/gmail/unmacaque/spring/ai/chat/ApplicationTest.java
+++ projects/ai/chat/src/test/java/com/gmail/unmacaque/spring/ai/chat/ApplicationTest.java
@@ -1,11 +1,11 @@
 package com.gmail.unmacaque.spring.ai.chat;
 
+import com.gmail.unmacaque.spring.ai.chat.web.AiController;
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,19 +15,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(AiController.class)
 class ApplicationTest {
 
 	@Autowired
 	private MockMvc mvc;
 
 	@MockitoBean(answers = Answers.RETURNS_DEEP_STUBS)
-	private ChatClient chatClient;
+	private ChatClient.Builder chatClientBuilder;
 
 	@Test
 	void testGenerate() throws Exception {
-		when(chatClient.prompt().user(anyString()).call().content()).thenReturn("Test");
+		when(chatClientBuilder.build().prompt().user(anyString()).call().content()).thenReturn("Test");
 
 		mvc.perform(get("/"))
 				.andExpectAll(
