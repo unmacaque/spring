f47918a1aca88b6c7ad807d28fee1098df8cbca8
Andreas Trepczik <andy@home.lab>
2025-11-21 14:53:45 +0100

    tentative removal of restdocs/restassured
    - https://github.com/spring-projects/spring-restdocs/issues/1000
    - https://github.com/rest-assured/rest-assured/issues/1846

--- projects/restdocs/restassured/src/main/java/com/gmail/unmacaque/spring/restdocs/restassured/domain/RegistrationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.unmacaque.spring.restdocs.restassured.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "registrations", path = "registrations")
-public interface RegistrationRepository extends PagingAndSortingRepository<Registration, Long>, CrudRepository<Registration, Long> {}
--- projects/restdocs/restassured/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into registration ( first_name, last_name, date ) values ('Fred', 'Flintstone', '2016-08-30 10:00:00.000Z');
-insert into registration ( first_name, last_name, date ) values ('Homer', 'Simpson', '2017-06-06 20:00:00.000Z');
