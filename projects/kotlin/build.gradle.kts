49322555c5b8baa988cf8af3eb374c86b8914f9f
Andreas Trepczik <andy@home.lab>
2025-11-26 16:58:32 +0100

    add spring-boot-h2console to all projects using h2

--- projects/kotlin/build.gradle.kts
+++ projects/kotlin/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     runtimeOnly("com.h2database:h2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testAndDevelopmentOnly("org.springframework.boot:spring-boot-h2console")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 }
