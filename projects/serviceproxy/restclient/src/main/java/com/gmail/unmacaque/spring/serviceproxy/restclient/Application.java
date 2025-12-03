ff7b77fc07e543e5efee8d6a4e248957893d85f1
Andreas Trepczik <andy@home.lab>
2025-12-03 15:57:46 +0100

    simplify restclient service example

--- projects/serviceproxy/restclient/src/main/java/com/gmail/unmacaque/spring/serviceproxy/restclient/Application.java
+++ projects/serviceproxy/restclient/src/main/java/com/gmail/unmacaque/spring/serviceproxy/restclient/Application.java
@@ -1,9 +1,12 @@
 package com.gmail.unmacaque.spring.serviceproxy.restclient;
 
+import com.gmail.unmacaque.spring.serviceproxy.restclient.domain.ReservationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.service.registry.ImportHttpServices;
 
 @SpringBootApplication
+@ImportHttpServices(types = ReservationService.class)
 public class Application {
 	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
