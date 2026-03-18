7dee785fb223c2db008394489e94522ea22c401d
Andreas Trepczik <andy@home.lab>
2026-03-18 17:50:04 +0100

    restore togglz project

---
+++ projects/thirdparty/togglz/src/main/java/com/gmail/unmacaque/spring/thirdparty/togglz/togglz/TogglzFeature.java
@@ -0,0 +1,19 @@
+package com.gmail.unmacaque.spring.thirdparty.togglz.togglz;
+
+import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
+import org.togglz.core.annotation.Label;
+
+public enum TogglzFeature implements Feature {
+
+	@EnabledByDefault
+	@Label("Feature A")
+	FEATURE_A,
+
+	@Label("Feature B")
+	FEATURE_B,
+
+	@Label("Feature C")
+	FEATURE_C,
+
+}
